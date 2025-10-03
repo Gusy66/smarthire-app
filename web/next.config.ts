fddb54a62f67d067ed79ff9c76a9a6812f0ba41a
Gusy66 <gustavobocci@hotmail.com>
2025-10-02 23:24:00 -0300

    arrumando package.json para upload na cloudflare

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
+import { withCloudflare } from "@cloudflare/next-on-pages/next";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default withCloudflare(nextConfig);
